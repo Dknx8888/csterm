08511f5f3c7ac452f02b26ceca8ef2aab9a3c75b
Dknx8888 <nick.dev.8888@gmail.com>
2026-07-22 19:33:39 -0400

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,9 +2,13 @@ class Program
 {
     static void Main()
     {
-        Console.Write("$ ");
-        var command = Console.ReadLine()?.Trim();
+        while (true)
+        {
+            Console.Write("$ ");
+            var command = Console.ReadLine()?.Trim();
         
-        Console.WriteLine($"{command}: command not found");
+            Console.WriteLine($"{command}: command not found");
+        }
+
     }
 }
