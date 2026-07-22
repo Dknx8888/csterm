c1c81083c53b6ffcfa1079981a62d0adddc5f4e3
Dknx8888 <nick.dev.8888@gmail.com>
2026-07-22 19:31:15 -0400

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,5 +3,8 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        var command = Console.ReadLine()?.Trim();
+        
+        Console.WriteLine($"{command}: command not found");
     }
 }
