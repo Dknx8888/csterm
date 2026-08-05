b9a524ff4d45c79b4a411c9d03b661adc037ead7
Dknx8888 <nick.dev.8888@gmail.com>
2026-08-05 19:11:56 -0400

    codecrafters submit [skip ci]

--- src/Helpers/ParseInput.cs
+++ src/Helpers/ParseInput.cs
@@ -4,25 +4,36 @@ namespace CodeCrafters.Shell.Helpers;
 
 public static class ParseInput
 {
+    private const char SingleQuote = '\'';
+    private const char DoubleQuote = '\"';
+    
     public static string[] Parse(string input)
     {
         var parts = new List<string>(); // parts = [command, arg1, arg2, ...]
         var currentPart = new StringBuilder(); // To repeatedly add chars without creating a new str each time
 
         var insideSingleQuotes = false;
+        var insideDoubleQuotes = false;
         var partStarted = false;
 
         foreach (var c in input)
         {
-            if (c == '\'')
+            if (c == DoubleQuote && !insideSingleQuotes)
+            {
+                insideDoubleQuotes = !insideDoubleQuotes;
+                partStarted = true;
+                continue;
+            }
+            
+            if (c == SingleQuote && !insideDoubleQuotes)
             {
                 insideSingleQuotes = !insideSingleQuotes; // Flip for start and end quotes
                 partStarted = true;
                 continue;
             }
             
-            // Meets white space that is not inside '' => one new arg => to parts
-            if (char.IsWhiteSpace(c) && !insideSingleQuotes)
+            // Meets white space that is not inside '' or "" => one new arg => to parts
+            if (char.IsWhiteSpace(c) && !insideSingleQuotes && !insideDoubleQuotes)
             {
                 if (partStarted)
                 {
@@ -40,6 +51,11 @@ public static class ParseInput
         if (insideSingleQuotes)
         {
             throw new FormatException("Unterminated single quote");
+        }
+
+        if (insideDoubleQuotes)
+        {
+            throw new FormatException("Unterminated double quote");
         }
 
         // Handle last arg that has no space after
